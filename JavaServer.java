325953d50dd4352e877c2b34df3d435536aa6c9a
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-20 00:17:18 +0800

    Update JavaServer.java

--- JavaServer.java
+++ JavaServer.java
@@ -8,7 +8,7 @@ ServerSocket ss=new ServerSocket(5506);
 Socket s=ss.accept();//establishes connection   
 DataInputStream dis=new DataInputStream(s.getInputStream());  
 String  str=(String)dis.readUTF();  
-System.out.println("Annyeonghaseyo "+str);  
+System.out.println("message : annyeong "+str);  
 ss.close();  
 }catch(Exception e){System.out.println(e);}  
 }  
