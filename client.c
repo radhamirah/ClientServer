8eb0e16cac9a2fb447146515b73dccd63356976e
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-20 02:56:37 +0800

    Update client.c

--- client.c
+++ client.c
@@ -3,36 +3,37 @@
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
-#include<string.h>
-#include<unistd.h>
-#include<netdb.h>
-#include<errno.h>
-
+ 
 int main()
 {
-  int sock, bytes received;
-  char send data[1024],recv_data[1024];
-  struct hosten *host;
-  struct sockaddr_in server_addr ;
-  
-  host=gethostbyname("192.168.31.132");
-  
-  if((sock=socket(AF_INET, SOCK_STREAM, 0)) == -1)
-  {
-    perror("Socket");
-    exit(1);
-  }
-  
-  server_addr.sin_family=AF_INET;
-  server_addr.sin_port=htons(6666);
-  server_addr.sin_addr=*((struct in_addr *)host ->h_addr);
-  bzero(&(server_addr.sin_zero),8);
-  
-  if(connect(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
-  {
-    perrot("Hello");
-    exit(1);
-  )
+    int network_socket;
+    network_socket=socket(AF_INET, SOCK_STREAM, 0);
+ 
+    struct sockaddr_in server_address;
+    server_address.sin_family=AF_INET;
+    server_address.sin_port=htons(6666);
+    server_address.sin_addr.s_addr=inet_addr("192.168.31.132");
+ 
+    int connect_socket=connect(network_socket,(struct sockaddr*)&server_address,sizeof(server_address));
+    if(connect_socket==-1)
+    {
+        printf("Error connecting socket");
+        exit(1);
+    }
+    //socket connected to the server
+ 
+    char ch[1024];
+     
+    //recieve data from server
+    while(1){
+    recv(network_socket, &ch, sizeof(ch), 0);
+    printf("The recieved data from the server is:\n%s",ch);
+    gets(ch);
+    send(network_socket,ch,strlen(ch),0);}
+     
+    //close(sock);
+return 0;
+}   
   
   
 
