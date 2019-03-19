3e5a56dc50c7ebccddae6b59fbb86ada0323ae1c
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-20 02:15:43 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,29 +1,38 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <stdlib.h>
-#include <arpa/inet.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<errno.h>
 
-int main(){
-  int clientSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  socklen_t addr_size;
-
-  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+int main()
+{
+  int sock, bytes received;
+  char send data[1024],recv_data[1024];
+  struct hosten *host;
+  struct sockaddr_in server_addr ;
   
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(6666);
-  serverAddr.sin_addr.s_addr = inet_addr("192.168.31.132");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+  host=gethostbyname("192.168.31.132");
+  
+  if((sock=socket(AF_INET, SOCK_STREAM, 0)) == -1)
+  {
+    perror("Socket");
+    exit(1);
+  }
+  
+  server_addr.sin_family=AF_INET;
+  server_addr.sin_port=htons(6666);
+  server_addr.sin_addr=*((struct in_addr *)host ->h_addr);
+  bzero(&(server_addr.sin_zero),8);
+  
+  if(connect(sock, (struct sockaddr *)&server_addr, sizeof(struct sockaddr)) == -1)
+  {
+    perrot("Hello");
+    exit(1);
+  )
+  
+  
 
-  addr_size = sizeof serverAddr;
-  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
-
-  recv(clientSocket, buffer, 1024, 0);
-
-  printf("Data received: %s",buffer);   
-
-  return 0;
-}
