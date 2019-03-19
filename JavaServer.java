e40d814522813c83945c76a5ab53f8146f143a86
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-20 00:45:25 +0800

    Update JavaServer.java

--- JavaServer.java
+++ JavaServer.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.net.*;  
 public class JavaServer {  
 public static void main(String[] args){  
-try{  
 ServerSocket ss=new ServerSocket(5506);  
 Socket s=ss.accept();//establishes connection   
 DataInputStream dis=new DataInputStream(s.getInputStream());  
