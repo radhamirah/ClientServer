39bba79c9a924af37251e02aeffd5b184028da29
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-20 04:27:09 +0800

    Update TcpServer.java

--- TcpServer.java
+++ TcpServer.java
@@ -1,97 +1,34 @@
-Java
 import java.io.*;
 import java.net.*;
-public class GossipClient
+
+public class TcpServer
 {
   public static void main(String[] args) throws Exception
   {
-     Socket sock = new Socket("127.0.0.1", 3000);
-                               // reading from keyboard (keyRead object)
-     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-                              // sending to client (pwrite object)
-     OutputStream ostream = sock.getOutputStream(); 
-     PrintWriter pwrite = new PrintWriter(ostream, true);
-
-                              // receiving from server ( receiveRead  object)
-     InputStream istream = sock.getInputStream();
-     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
-
-     System.out.println("Start the chitchat, type and press Enter key");
-
-     String receiveMessage, sendMessage;               
-     while(true)
-     {
-        sendMessage = keyRead.readLine();  // keyboard reading
-        pwrite.println(sendMessage);       // sending to server
-        pwrite.flush();                    // flush the data
-        if((receiveMessage = receiveRead.readLine()) != null) //receive from server
-        {
-            System.out.println(receiveMessage); // displaying at DOS prompt
-        }         
-      }               
-    }                    
-}                        
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
-27
-28
-29
-30
-31
-32
-import java.io.*;
-import java.net.*;
-public class GossipClient
-{
-  public static void main(String[] args) throws Exception
-  {
-     Socket sock = new Socket("192.168.31.132", 6666);
-                               // reading from keyboard (keyRead object)
-     BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-                              // sending to client (pwrite object)
-     OutputStream ostream = sock.getOutputStream(); 
-     PrintWriter pwrite = new PrintWriter(ostream, true);
+      ServerSocket sersock = new ServerSocket(6666);
+      System.out.println("Annyeong");
+      Socket sock = sersock.accept( );                          
+                              // reading from keyboard (keyRead object)
+      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+	                      // sending to client (pwrite object)
+      OutputStream ostream = sock.getOutputStream(); 
+      PrintWriter pwrite = new PrintWriter(ostream, true);
  
                               // receiving from server ( receiveRead  object)
-     InputStream istream = sock.getInputStream();
-     BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
+      InputStream istream = sock.getInputStream();
+      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
  
-     System.out.println("Annyeong");
- 
-     String receiveMessage, sendMessage;               
-     while(true)
-     {
-        sendMessage = keyRead.readLine();  // keyboard reading
-        pwrite.println(sendMessage);       // sending to server
-        pwrite.flush();                    // flush the data
-        if((receiveMessage = receiveRead.readLine()) != null) //receive from server
+      String receiveMessage, sendMessage;               
+      while(true)
+      {
+        if((receiveMessage = receiveRead.readLine()) != null)  
         {
-            System.out.println(receiveMessage); // displaying at DOS prompt
+           System.out.println(receiveMessage);         
         }         
+        sendMessage = keyRead.readLine(); 
+        pwrite.println(sendMessage);             
+        pwrite.flush();
       }               
     }                    
-}      
+}         
+
