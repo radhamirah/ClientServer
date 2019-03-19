15e12e560e3aee6139d328a2f18a693aff2293d6
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-20 00:56:11 +0800

    Update JavaServer.java

--- JavaServer.java
+++ JavaServer.java
@@ -2,12 +2,14 @@ import java.io.*;
 import java.net.*;  
 public class JavaServer {  
 public static void main(String[] args){  
+try{  
 ServerSocket ss=new ServerSocket(5506);  
 Socket s=ss.accept();//establishes connection   
 DataInputStream dis=new DataInputStream(s.getInputStream());  
 String  str=(String)dis.readUTF();  
-System.out.println("Annyeong "+str);  
+System.out.println("Annyeong= "+str);  
 ss.close();  
-} 
+}catch(Exception e){System.out.println(e);}  
 }  
+}
 
