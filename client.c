e6ad9f65fe66862003aebcec15c991204dc0f6b3
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-20 14:18:34 +0800

    Update client.c

--- client.c
+++ client.c
@@ -1,37 +1,63 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h>
 #include <arpa/inet.h>
-#include <unistd.h>
- 
-int main()
-{
-    int network_socket;
-    network_socket=socket(AF_INET, SOCK_STREAM, 0);
- 
- 
-    struct sockaddr_in server_address;
-    server_address.sin_family=AF_INET;
-    server_address.sin_port=htons(6666);
-    server_address.sin_addr.s_addr=inet_addr("192.168.31.132");
- 
-    int connect_socket=connect(network_socket,(struct sockaddr*)&server_address,sizeof(server_address));
-    if(connect_socket==-1)
-    {
-         printf("error");
-    }
-  
-     char server_response[256];
-     recv(network_socket, &server_response, sizeof(server_response), 0);
-   
-     printf("Hello! %s\n", server_response);
-     
-    close(network_socket);
- 
-    return 0;
-}   
+#define PORT 6666
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = inet_addr("192.168.31.132"); 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+    valread = read( new_socket , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    send(new_socket , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+    return 0; 
+} 
+
   
   
 
