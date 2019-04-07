f87c35627b70fa946ebb5a5053d5dd330dbb22ef
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-04-07 12:33:09 +0800

    Update newClient.c

--- newClient.c
+++ newClient.c
@@ -1,63 +1,63 @@
-#include <arpa/inet.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h> 
 
-int main() {
-	const char* server_name = "localhost";
-	const int server_port = 6606;
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
 
-	struct sockaddr_in server_address;
-	memset(&server_address, 0, sizeof(server_address));
-	server_address.sin_family = AF_INET;
+int main(int argc, char *argv[])
+{
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-	// creates binary representation of server name
-	// and stores it as sin_addr
-	// http://beej.us/guide/bgnet/output/html/multipage/inet_ntopman.html
-	inet_pton(AF_INET, server_name, &server_address.sin_addr);
-
-	// htons: port in network order format
-	server_address.sin_port = htons(server_port);
-
-	// open a stream socket
-	int sock;
-	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) < 0) {
-		printf("could not create socket\n");
-		return 1;
-	}
-
-	// TCP is connection oriented, a reliable connection
-	// **must** be established before any data is exchanged
-	if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
-		printf("could not connect to server\n");
-		return 1;
-	}
-
-	// send
-
-	// data that will be sent to the server
-	const char* data_to_send = "Hai Server";
-	send(sock, data_to_send, strlen(data_to_send), 0);
-
-	// receive
-
-	int n = 0;
-	int len = 0, maxlen = 100;
-	char buffer[maxlen];
-	char* pbuffer = buffer;
-
-	// will remain open until the server terminates the connection
-	while ((n = recv(sock, pbuffer, maxlen, 0)) > 0) {
-		pbuffer += n;
-		maxlen -= n;
-		len += n;
-
-		buffer[len] = '\0';
-		printf("received: '%s'\n", buffer);
-	}
-
-	// close the socket
-	close(sock);
-	return 0;
+    char buffer[256];
+    if (argc < 3) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+    serv_addr.sin_port = htons(portno);
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+    printf("Client: ");
+    while(1)
+    {
+        bzero(buffer,255);
+        fgets(buffer,255,stdin);
+        n = write(sockfd,buffer,strlen(buffer));
+        if (n < 0) 
+             error("ERROR writing to socket");
+        bzero(buffer,255);
+        n = read(sockfd,buffer,255);
+        if (n < 0) 
+             error("ERROR reading from socket");
+        printf("Server : %s\n",buffer);
+        int i = strncmp("Bye" , buffer , 3);
+        if(i == 0)
+               break;
+    }
+    close(sockfd);
+    return 0;
 }
