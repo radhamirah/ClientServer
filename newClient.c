3a914af4e8702690ffbf38f9395f5ea1a0f6bf65
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-04-07 11:16:21 +0800

    Update newClient.c

--- newClient.c
+++ newClient.c
@@ -1,65 +1,38 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h> 
 #include <sys/socket.h>
-#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
 
-void error(char *msg); /* function prototype */
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
-}
+int main(){
+  int clientSocket;
+  char buffer[1024];
+  struct sockaddr_in serverAddr;
+  socklen_t addr_size;
 
-int main(int argc, char *argv[])
-{
-     int sockfd, portno, n;
-     struct sockaddr_in serv_addr;
-     struct hostent *server;
-     
-     char buffer[256];
-     if (argc < 3)
-     { 
-         fprintf(stderr,"usage % 6606", argc[0]);
-         exit(0);
-     }
-     portno = atoi(argv[2]);
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0)
-         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
-    if (server == NULL)
-    {
-        fprintf(stderr,"ERROR, no such host");
-        exit (0);
-    }
-    
-    struct hostent *gethostname(char *name)
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-    
-    void bcopy(char *s1, char *s2, int length)
-    if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0)
-        error("ERROR connecting");
-    
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
-    n = writer(sockfd,buffer,strlen(buffer));
-    if (n < 0)
-        error("ERROR writing to sokcet");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0)
-        error("ERROR reading from socket");
-    printf("%s ",buffer);
-    return 0;
+  /*---- Create the socket. The three arguments are: ----*/
+  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
+  clientSocket = socket(PF_INET, SOCK_STREAM, 0);
+  
+  /*---- Configure settings of the server address struct ----*/
+  /* Address family = Internet */
+  serverAddr.sin_family = AF_INET;
+  /* Set port number, using htons function to use proper byte order */
+  serverAddr.sin_port = htons(6606);
+  /* Set IP address to localhost */
+  serverAddr.sin_addr.s_addr = inet_addr("192.168.31.144");
+  /* Set all bits of the padding field to 0 */
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
+
+  /*---- Connect the socket to the server using the address struct ----*/
+  addr_size = sizeof serverAddr;
+  connect(clientSocket, (struct sockaddr *) &serverAddr, addr_size);
+
+  /*---- Read the message from the server into the buffer ----*/
+  recv(clientSocket, buffer, 1024, 0);
+
+  /*---- Print the received message ----*/
+  printf("Data received: %s",buffer);   
+
+  return 0;
 }
-    
-   
-          
           
