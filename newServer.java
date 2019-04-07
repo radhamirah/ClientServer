d2986fa17048c4e02a6a65f4336e7ace06fd823e
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-04-07 12:42:06 +0800

    Update newServer.java

--- newServer.java
+++ newServer.java
@@ -1,32 +1,61 @@
-import java.io.*;
-import java.net.*;
-public class newServer
-{
-  public static void main(String[] args) throws Exception
-  {
-      ServerSocket sersock = new ServerSocket(6606);
-      System.out.println("Server  ready for chatting");
-      Socket sock = sersock.accept( );                          
-                              // reading from keyboard (keyRead object)
-      BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
-	                      // sending to client (pwrite object)
-      OutputStream ostream = sock.getOutputStream(); 
-      PrintWriter pwrite = new PrintWriter(ostream, true);
- 
-                              // receiving from server ( receiveRead  object)
-      InputStream istream = sock.getInputStream();
-      BufferedReader receiveRead = new BufferedReader(new InputStreamReader(istream));
- 
-      String receiveMessage, sendMessage;               
-      while(true)
-      {
-        if((receiveMessage = receiveRead.readLine()) != null)  
-        {
-           System.out.println(receiveMessage);         
-        }         
-        sendMessage = keyRead.readLine(); 
-        pwrite.println(sendMessage);             
-        pwrite.flush();
-      }               
-    }                    
-}                        
+import java.net.*; 
+import java.io.*; 
+  
+class newServer 
+{ 
+    //initialize socket and input stream 
+    private Socket          socket   = null; 
+    private ServerSocket    server   = null; 
+    private DataInputStream in       =  null; 
+  
+    // constructor with port 
+    public GetServer(int port) 
+    { 
+        // starts server and waits for a connection 
+        try
+        { 
+            server = new ServerSocket(port); 
+            System.out.println("Server started"); 
+  
+            System.out.println("tunggu client jap"); 
+  
+            socket = server.accept(); 
+            System.out.println("Client dah accept"); 
+  
+            // takes input from the client socket 
+            in = new DataInputStream( 
+                new BufferedInputStream(socket.getInputStream())); 
+  
+            String line = ""; 
+  
+            // reads message from client until "Over" is sent 
+            while (!line.equals("Over")) 
+            { 
+                try
+                { 
+                    line = in.readUTF(); 
+                    System.out.println(line); 
+  
+                } 
+                catch(IOException i) 
+                { 
+                    System.out.println(i); 
+                } 
+            } 
+            System.out.println("Closing connection"); 
+  
+            // close connection 
+            socket.close(); 
+            in.close(); 
+        } 
+        catch(IOException i) 
+        { 
+            System.out.println(i); 
+        } 
+    } 
+  
+    public static void main(String args[]) 
+    { 
+        newServer server = new newServer(6606); 
+    } 
+} 
