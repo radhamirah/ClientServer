f69e609e0416c3601572ad410b077150b412d815
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-04-07 11:04:41 +0800

    Update newClient.c

--- newClient.c
+++ newClient.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h> 
+#include <netdb.h>
 
+void error(char *msg); /* function prototype */
 void error(const char *msg)
 {
     perror(msg);
@@ -15,49 +15,51 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-
-    char buffer[256];
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-    portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
+     int sockfd, portno, n;
+     struct sockaddr_in serv_addr;
+     struct hostent *server;
+     
+     char buffer[256];
+     if (argc < 3)
+     { 
+         fprintf(stderr,"usage % 6606", argc[0]);
+         exit(0);
+     }
+     portno = atoi(argv[2]);
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     if (sockfd < 0)
+         error("ERROR opening socket");
     server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+    if (server == NULL)
+    {
+        fprintf(stderr,"ERROR, no such host");
+        exit (0);
     }
+    
+    struct hostent *gethostname(char *name)
     bzero((char *) &serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    
+    void bcopy(char *s1, char *s2, int length)
+    if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-    printf("Client: ");
-    while(1)
-    {
-        bzero(buffer,255);
-        fgets(buffer,255,stdin);
-        n = write(sockfd,buffer,strlen(buffer));
-        if (n < 0) 
-             error("ERROR writing to socket");
-        bzero(buffer,255);
-        n = read(sockfd,buffer,255);
-        if (n < 0) 
-             error("ERROR reading from socket");
-        printf("Server : %s\n",buffer);
-        int i = strncmp("Bye" , buffer , 3);
-        if(i == 0)
-               break;
-    }
-    close(sockfd);
+    
+    printf("Please enter the message: ");
+    bzero(buffer,256);
+    fgets(buffer,255,stdin);
+    n = writer(sockfd,buffer,strlen(buffer));
+    if (n < 0)
+        error("ERROR writing to sokcet");
+    bzero(buffer,256);
+    n = read(sockfd,buffer,255);
+    if (n < 0)
+        error("ERROR reading from socket");
+    printf("%s ",buffer);
     return 0;
 }
+    
+   
+          
+          
